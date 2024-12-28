9b044df8ca0b9d461abc30f2bd39d405b785ed32
luciuswilbert <luciuswilbert@gmail.com>
2024-12-28 14:27:12 +0800

    Finally after 1.5 days

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["Fullstack Developer", "Fulltime BF of Jesslyn Santoso"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
