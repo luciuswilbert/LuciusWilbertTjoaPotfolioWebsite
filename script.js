e932ede3603c3d7365da02bfd6bdc92554f4d384
luciuswilbert <luciuswilbert@gmail.com>
2024-12-30 11:04:36 +0800

    HM

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /* typing animation */
-var typed = new Typed(".typing-home", {
+var typedHome = new Typed(".typing-home", {
     strings:["Fullstack Developer", "Web Developer", "Mobile App Engineer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
 })
 
-var typed = new Typed(".typing-about", {
+var typedAbout = new Typed(".typing-about", {
     strings:["Fullstack Developer", "Web Developer", "Mobile App Engineer"],
     typeSpeed:100,
     BackSpeed:60,
