9333ad4e03a613606d0c2980a14e7facbc474213
luciuswilbert <luciuswilbert@gmail.com>
2025-04-19 20:03:24 +0800

    Awards Section, new Recent Project

--- script.js
+++ script.js
@@ -22,3 +22,44 @@ function scrollToContact(subject) {
     const subjectField = document.getElementById('subject');
     subjectField.value = subject;
 }
+
+
+
+
+let devmatchImages = [
+    "images/devmatch1.jpg",
+    "images/devmatch2.jpg",
+];
+let devmatchIndex = 0;
+let devmatchInterval;
+
+function startDevMatchSlider() {
+    const img = document.getElementById("devmatch-img");
+    devmatchInterval = setInterval(() => {
+        devmatchIndex = (devmatchIndex + 1) % devmatchImages.length;
+        img.src = devmatchImages[devmatchIndex];
+    }, 1000);
+}
+
+function stopDevMatchSlider() {
+    clearInterval(devmatchInterval);
+}
+
+let jogetImages = [
+    "images/joget1.jpg",
+    "images/joget2.jpg"
+];
+let jogetIndex = 0;
+let jogetInterval;
+
+function startJogetSlider() {
+    const img = document.getElementById("joget-img");
+    jogetInterval = setInterval(() => {
+        jogetIndex = (jogetIndex + 1) % jogetImages.length;
+        img.src = jogetImages[jogetIndex];
+    }, 1000);
+}
+
+function stopJogetSlider() {
+    clearInterval(jogetInterval);
+}
